dae0165578ba0f4a891a9c1f96b3d29bb03be067
Juanfran <juanfran.alcantara@kaleidos.net>
2015-01-16 14:54:56 +0100

    7c2550587b4d32839bf9b16bb6209573167dc9e7

--- dist.js
+++ dist.js
@@ -50,6 +50,6 @@ exec(action)
     })
     .then(function() {
         //push
-        return exec('git push --repo https://' + params[0] + ':' + params[1] + '@github.com:taigaio/taiga-front-dist.git master');
+        return exec('git push origin master');
     })
     .done();
