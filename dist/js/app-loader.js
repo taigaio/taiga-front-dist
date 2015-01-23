150bebeb7405577f40e2e3f9517f7a6471cf0fe6
Jesús Espino <jespinog@gmail.com>
2015-01-23 09:13:01 +0100

    5b8da33b797ef7bcbd5987515626bbccc0582a1f

---
+++ dist/js/app-loader.js
@@ -0,0 +1,43 @@
+(function() {
+  var promise, version;
+
+  version = 1422000753297;
+
+  window.taigaConfig = {
+    "api": "http://localhost:8000/api/v1/",
+    "eventsUrl": null,
+    "debug": true,
+    "publicRegisterEnabled": true,
+    "feedbackEnabled": true,
+    "privacyPolicyUrl": null,
+    "termsOfServiceUrl": null,
+    "maxUploadFileSize": null,
+    "gitHubClientId": null,
+    "contribPlugins": []
+  };
+
+  promise = $.getJSON("/js/conf.json");
+
+  promise.done(function(data) {
+    return window.taigaConfig = _.extend({}, window.taigaConfig, data);
+  });
+
+  promise.always(function() {
+    var plugins;
+    if (window.taigaConfig.contribPlugins.length > 0) {
+      plugins = _.map(window.taigaConfig.contribPlugins, function(plugin) {
+        return "" + plugin + "?v=" + version;
+      });
+      return ljs.load(plugins, function() {
+        return ljs.load("/js/app.js?v=" + version, function() {
+          return angular.bootstrap(document, ['taiga']);
+        });
+      });
+    } else {
+      return ljs.load("/js/app.js?v=" + version, function() {
+        return angular.bootstrap(document, ['taiga']);
+      });
+    }
+  });
+
+}).call(this);
