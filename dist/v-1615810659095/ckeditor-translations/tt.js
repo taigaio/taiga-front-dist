919f598ed97e590f7373bae7a4e5f5cc58211f51
Yamila Moreno <yamila.ms@gmail.com>
2021-03-15 13:19:39 +0100

    6341af476242447e2b0e323f79b02f2d96960f1a

---
+++ dist/v-1615810659095/ckeditor-translations/tt.js
@@ -0,0 +1,1 @@
+!function(t){const n=t.tt=t.tt||{};n.dictionary=Object.assign(n.dictionary||{},{Bold:"Калын",Cancel:"",Code:"Код",Italic:"",Redo:"Кабатла",Save:"Сакла",Strikethrough:"",Undo:""}),n.getPluralForm=function(t){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
