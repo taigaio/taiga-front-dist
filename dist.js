5953f76bf3cfe4b00594955557ebf7660e46d1ee
David Barragán Merino <david.barragan@kaleidos.net>
2023-03-01 14:08:17 +0100

    chore: upgrade dependencies

--- dist.js
+++ dist.js
@@ -6,11 +6,13 @@
  * Copyright (c) 2021-present Kaleidos Ventures SL
  */
 
-var Promise = require("bluebird");
-var fs = require("fs");
-var delAsync = Promise.promisify(require("del"));
-var exec = Promise.promisify(require('child_process').exec);
-var ncp = Promise.promisify(require('ncp').ncp);
+import bluebird from "bluebird";
+import * as fs from "fs";
+import { exec } from 'child_process';
+import { deleteAsync } from "del";
+
+var _exec = bluebird.promisify(exec);
+var _del = deleteAsync;
 
 var local = 'tmp';
 var repo = 'https://github.com/kaleidos-ventures/taiga-front';
@@ -23,54 +25,45 @@ if (process.argv.length !== 3){
 var branch = process.argv[2];
 
 var synchRepoAction = (function cloneOrPull(){
-    var cloned = fs.existsSync(local);
-
-    if (cloned) {
-        action = 'git checkout ' + branch + ' && cd ' + local + ' && git checkout ' + branch + ' && git pull'
-    } else {
-        action = 'git checkout ' + branch + ' && git clone -b ' + branch + '  ' + repo + ' ' + local;
-    }
-
-    return action;
+    return fs.existsSync(local)
+        ? 'git checkout ' + branch + ' && cd ' + local + ' && git checkout ' + branch + ' && git pull'
+        : 'git checkout ' + branch + ' && git clone -b ' + branch + '  ' + repo + ' ' + local;
 }())
 
-exec(synchRepoAction)
+_exec(synchRepoAction)
     .then(function() {
         console.log("remove old tmp dist")
         //remove old tmp dist
-        return delAsync(local + '/dist');
+        return _del(local + '/dist');
     })
     .then(function() {
         console.log("compile taiga")
         //compile taiga
-        return exec('cd ' + local + ' && npm ci && npx gulp deploy');
+        return _exec('cd ' + local + ' && npm ci && npx gulp deploy');
     })
     .then(function() {
         console.log("remove old dist")
         //remove old dist
-        return delAsync('dist');
+        return _del('dist');
     })
     .then(function() {
         console.log("copy new dist")
         //copy new dist
-        //return ncp(local + '/dist/', 'dist');
-        return exec('cp -r ' + local + '/dist/ dist');
+        return _exec('cp -r ' + local + '/dist/ dist');
     })
     .then(function() {
         console.log("get last commit id")
         //get last commit id
-        return exec('cd ' + local + ' && git rev-parse HEAD');
+        return _exec('cd ' + local + ' && git rev-parse HEAD');
     })
     .then(function(lastCommitId) {
         console.log("commit")
         //commit
-        lastCommitId = lastCommitId[0].trim();
-
-        return exec('git add -A && git commit -am "' + lastCommitId + '"');
+        return _exec('git add -A && git commit -am "' + lastCommitId.trim() + '"');
     })
     // .then(function() {
     //     console.log("push")
     //     //push
-    //     return exec('git push origin ' + branch);
+    //     return _exec('git push origin ' + branch);
     // })
     .done();
